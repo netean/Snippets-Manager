788e5ec483e18db8cc1ba4ed8f14d39587597431
netean <netean@gmail.com>
2025-09-10 10:24:39 +0100

    removed all hard coded file paths, now eveything uses relative paths so should work across multiple devices

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_snippetmanager_FILE /home/iain/Dev/snippets/build/snippetmanager)
-set(__QT_DEPLOY_TARGET_snippetmanager_TYPE EXECUTABLE)
--- build/CMakeFiles/snippetmanager.dir/snippetmanager_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/snippetmanager.dir/snippetmanager_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/iain/Dev/snippets/build/snippetmanager_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
